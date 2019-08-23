c7575783f9d0d2792ff7e1b9cad207a2ce909a7c
Tudor <tudor.carnaru@gmail.com>
2019-08-23 23:09:53 +0300

    Missed constants class in last commit

---
+++ src/main/FizzBuzzConstants.java
@@ -0,0 +1,5 @@
+public class FizzBuzzConstants {
+    public static final String FIZZ_STRING = "fizz";
+    public static final String BUZZ_STRING = "buzz";
+    public static final String FIZZ_BUZZ_STRING = "fizzbuzz";
+}
