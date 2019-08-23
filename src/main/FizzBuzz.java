0de989edf501fef552a4e1000529d76d0cb0ed13
Tudor <tudor.carnaru@gmail.com>
2019-08-23 23:01:13 +0300

    Finished Task 1 : Implementing FizzBuzz Algorithm

--- src/main/FizzBuzz.java
+++ src/main/FizzBuzz.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class FizzBuzz {
+
+    public List<String> doFizzBuzz(Integer lowerRange, Integer upperRange) {
+            List<String> fizzBuzzOutput = new ArrayList<>();
+            for (int i = lowerRange; i<= upperRange; i++) {
+                String fizzBuzzResult = checkFizzBuzz(i);
+                fizzBuzzOutput.add(fizzBuzzResult);
+            }
+            return fizzBuzzOutput;
+    }
+
+    public String checkFizzBuzz(Integer numberToCheck) {
+            if(numberToCheck % 3 == 0 && numberToCheck % 5 != 0) return FizzBuzzConstants.FIZZ_STRING;
+            else if (numberToCheck % 5 == 0 && numberToCheck % 3 != 0) return FizzBuzzConstants.BUZZ_STRING;
+            else if (numberToCheck % 15 == 0) return FizzBuzzConstants.FIZZ_BUZZ_STRING;
+            else return numberToCheck.toString();
+    }
 }
--- src/tests/FizzBuzzTest.java
+++ src/tests/FizzBuzzTest.java
@@ -1,5 +1,31 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTest {
+
+    FizzBuzz fizzBuzz = new FizzBuzz();
+
+    @Test
+    public void testDiv15() {
+        assertEquals(fizzBuzz.checkFizzBuzz(15), FizzBuzzConstants.FIZZ_BUZZ_STRING);
+    }
+
+    @Test
+    public void testDiv5() {
+        assertEquals(fizzBuzz.checkFizzBuzz(5), FizzBuzzConstants.BUZZ_STRING);
+    }
+
+    @Test
+    public void testDiv3() {
+        assertEquals(fizzBuzz.checkFizzBuzz(3), FizzBuzzConstants.FIZZ_STRING);
+    }
+
+    @Test
+    public void testRange() {
+        assertEquals(fizzBuzz.doFizzBuzz(1, 20).get(14), FizzBuzzConstants.FIZZ_BUZZ_STRING);
+        assertEquals(fizzBuzz.doFizzBuzz(1, 20).get(2), FizzBuzzConstants.FIZZ_STRING);
+        assertEquals(fizzBuzz.doFizzBuzz(1, 20).get(4), FizzBuzzConstants.BUZZ_STRING);
+    }
 
 }
